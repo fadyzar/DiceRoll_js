a57144915268560d69c23fc060f5909673db84b7
fadyZar <93481957+fadyzar@users.noreply.github.com>
2023-10-08 03:50:25 +0300

    Add files via upload

---
+++ Dice Roll/index.js
@@ -0,0 +1,9 @@
+//Select button start game
+
+const windowBtn = document.querySelector(".btn1")
+
+
+windowBtn.addEventListener("click", function(e){
+    // window.open("./main.html");
+    window.location.href = "./main.html";
+})
--- Dice Roll/main.js
+++ Dice Roll/main.js
@@ -1,14 +1,15 @@
 //select all Elements that we will use 
-
+let containerPlayers = document.querySelector(".playerDiv")
 //Select all images
+
 const firstDice = document.querySelector(".image1Div")//hold the first dice div (image)
 const secondDice = document.querySelector(".image2Div")//hold the second dice div(image)
 const gameStatusImg = document.querySelector(".dealerImg")//hold the game status image
 
 //Select all buttons
 const newGameBtn = document.querySelector(".btn1")//hold the new game button
-const rollDiceBtn = document.querySelector(".btn2")//hold the roll dice button
-const holdBtn = document.querySelector(".btn3")//hold the hold button
+let rollDiceBtn = document.querySelector(".btn2")//hold the roll dice button
+let holdBtn = document.querySelector(".btn3")//hold the hold button
 
 //Select score display for each player
 let player1Score = document.querySelector(".player1Score")//first player's score
@@ -16,11 +17,19 @@ let player2Score = document.querySelector(".player2Score")//second player's scor
 
 //current score
 let current1Score = document.querySelector(".current1Score")//current score for player 1
-let current2Score =  document.querySelector(".current2Score")//current score for player 2
+let current2Score = document.querySelector(".current2Score")//current score for player 2
+let changeCurrent1ToWinner = document.querySelector(".changeCurrent1ToWinner")
+let changeCurrent2ToWinner = document.querySelector(".changeCurrent2ToWinner")
 
 //Select two player
-let player_1 = document.getElementsByClassName("player1Div")//hold player 1
-let player_2 = document.getElementsByClassName("player2Div")//hold player 2
+let player1Color = document.querySelector(".player1Div")//hold player 1
+let player2Color = document.querySelector(".player2Div")//hold player 2
+let result1 = document.querySelector(".result1")
+let result2 = document.querySelector(".result2")
+
+
+
+
 
 //declare variables
 let score_1 = 0 //for score
@@ -33,6 +42,15 @@ let player1 = true
 let player2 = true
 
 
+let win1 = 0
+let win2 = 0
+
+
+
+
+
+
+
 
 //roll button
 rollDiceBtn.addEventListener("click", function (e) {
@@ -40,6 +58,7 @@ rollDiceBtn.addEventListener("click", function (e) {
 
     let firstDrop = 0;
     let secondDrop = 0;
+
 
     gameStatusImg.style.backgroundImage = "url('./images/dellear.webp')"
     const rand1 = Math.ceil(Math.random() * 6)
@@ -51,8 +70,11 @@ rollDiceBtn.addEventListener("click", function (e) {
             firstDice.style.backgroundImage = "url('./images/dice1.png')"
             if (player1 == true) {
                 score_1 += 1
+
             } else {
                 score_2 += 1
+
+
             }
 
             break;
@@ -60,6 +82,7 @@ rollDiceBtn.addEventListener("click", function (e) {
             firstDice.style.backgroundImage = "url('./images/dice2.png')"
             if (player1 == true) {
                 score_1 += 2
+
             } else {
                 score_2 += 2
             }
@@ -161,20 +184,21 @@ rollDiceBtn.addEventListener("click", function (e) {
             console.log(`Sorry, we are out of ${rand1}.`);
     }
 
-    console.log(`score 1 = ${score_1}`)
-    console.log(`score 2 = ${score_2}`)
+    // console.log(`score 1 = ${score_1}`)
 
-    if (firstDrop === 6 && secondDrop === 6) {
-        score_1 = 0;
-        score_2 = 0;
-        player1Score.innerText = score_1
-        player1Score.innerText = score_2
-        alert(`OOPS, you get double six `)
-        gameStatusImg.style.backgroundImage = "url('./images/sixdice.jpeg')"
-    }
 
-    player1Score.innerText = score_1
-    player2Score.innerText = score_2
+
+    // if (firstDrop === 6 && secondDrop === 6) {
+    //     score_1 = 0;
+    //     score_2 = 0;
+    //     // player1Score.innerText = score_1
+    //     // player1Score.innerText = score_2
+    //     alert(`OOPS, you get double six `)
+    //     gameStatusImg.style.backgroundImage = "url('./images/sixdice.jpeg')"
+    // }
+
+    current1Score.innerText = score_1
+    current2Score.innerText = score_2
 
 
 
@@ -182,24 +206,45 @@ rollDiceBtn.addEventListener("click", function (e) {
 
 //hold button
 holdBtn.addEventListener("click", function (e) {
-    console.log(`player1 = ${player1}`)
+    // console.log(`player1 = ${player1}`)
     if (player1 == true) {
+        let newCurrent1;
         console.log(score_1)
         current1 += score_1
-        current1Score.innerText = current1
-        score_1 = 0;
-        player1Score.innerText = score_1
+        newCurrent1 = current1
+        player1Score.innerText = newCurrent1
+       
         
+        console.log(`new = ${newCurrent1}`)
+        
+        checkWinner1(newCurrent1)
+        score_1 = 0;
+        current1Score.innerText = score_1
+
     } else {
+        let newCurrent2;
         console.log(score_2)
         current2 += score_2
-        current2Score.innerText = current2
+        newCurrent2 = current2
+        player2Score.innerText = newCurrent2
+        
+       
+       
+        console.log(`new = ${newCurrent2}`)
+        checkWinner2(newCurrent2)
         score_2 = 0;
-        player2Score.innerText = score_2
+        current2Score.innerText = score_2
 
     }
 
+
+
+
+
+
     activePlayer();
+    colorStylingChange();
+
 
 
 
@@ -210,12 +255,79 @@ newGameBtn.addEventListener("click", function (e) {
     location.reload()
 })
 
+
+//player turn (active player)
 function activePlayer() {
-if(player1 == true){
-    player1 =false
-}else
-player1 =true
+    if (player1 == true) {
+        player1 = false
+
+
+    } else
+        player1 = true
 }
 
+
+function colorStylingChange() {
+    if (player1 == true) {
+        player1Color.style.backgroundColor = "rgba(64, 55, 55, 0.356)"
+        player2Color.style.backgroundColor = "rgba(49, 34, 34, 0.523)"
+    } else {
+        player2Color.style.backgroundColor = "rgba(64, 55, 55, 0.356)"
+        player1Color.style.backgroundColor = "rgba(49, 34, 34, 0.523)"
+    }
+
+
+}
+
+console.log(`player1score = ${current1}`)
+console.log(`player1score = ${player1Score}`)
+console.log(`player1score = ${current1Score}`)
+console.log(`player1score = ${score_1}`)
+
+
+
+
+function checkWinner1(newCurrent1) {
+
+    if (newCurrent1 > 100) {
+        console.log(`newCurrent1 = ${newCurrent1}`)
+        
+        // containerPlayers.style.backgroundColor = "red"
+        changeCurrent2ToWinner.innerText = "YOU WIN :)"
+        gameStatusImg.style.backgroundImage = "url('./images/winnerGif1.gif')"
+        // disableButtonClick()
+        win2++;
+        result2.textContent = `Player 1 ---> ${win2}`
+        return true
+
+    }
+    
+}
+
+function checkWinner2(newCurrent2) {
+    // containerPlayers.style.backgroundColor = "red"
+    if (newCurrent2 > 100) {
+        win1++;
+        
+        result1.textContent = `Player 1 ---> ${win1}`
+        
+        changeCurrent1ToWinner.innerText = "COMPUTER WIN"
+        gameStatusImg.style.backgroundImage = "url('./images/lose.gif')"
+        // disableButtonClick()
+        return true
+
+    }
+    
+}
+
+
+function disableButtonClick() {
+
+    // holdBtn.style.display = "none"
+    // rollDiceBtn.style.display = "none"
+    // containerPlayers.style.display = "none"
+    // window.open("./newGame.html");
+
+}
 
 
